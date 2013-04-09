ca6d84bf5257e3d57a33fb45d433d736c0a29154
Pierre Durand <pierredurand@gmail.com>
2013-04-09 23:55:32 +0200

    first tests

---
+++ server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"time"
+	"io/ioutil"
+	"net/http"
+	"github.com/bradfitz/gomemcache/memcache"
+)
+
+var (
+	cache = memcache.New("localhost:11211")
+)
+
+func main() {
+	http.HandleFunc("/", handle)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	item, err := cache.Get("foo")
+	var bytes []byte
+	if err == nil {
+		bytes = item.Value
+	} else {
+		time.Sleep(200 * time.Millisecond)
+		bytes, _ = ioutil.ReadFile("google.jpg")
+		item = &memcache.Item{Key: "foo", Value: bytes}
+		cache.Set(item)
+	}
+	w.Write(bytes)
+}
+
