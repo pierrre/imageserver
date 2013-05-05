516c05bfbb80c5e186d1c881753d5e4b96814ae2
Pierre Durand <pierre@plyce.com>
2013-05-05 19:28:57 +0200

    parse request

--- server.go
+++ server.go
@@ -1,7 +1,11 @@
 package imageproxy
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
+	"net/url"
+	"strconv"
 )
 
 type Server struct {
@@ -17,11 +21,58 @@ func NewServer(httpServer *http.Server, cache Cache) *Server {
 }
 
 func (server *Server) Run() {
-	serveMux := http.NewServeMux();
-	serveMux.HandleFunc("/", server.handleHttpRequest);
+	serveMux := http.NewServeMux()
+	serveMux.HandleFunc("/", server.handleHttpRequest)
 	server.httpServer.Handler = serveMux
 	server.httpServer.ListenAndServe()
 }
 
-func (server *Server) handleHttpRequest(w http.ResponseWriter, r *http.Request) {
+func (server *Server) handleHttpRequest(writer http.ResponseWriter, request *http.Request) {
+	source, parameters, error := server.parseRequest(request)
+	fmt.Println(source, parameters, error)
+}
+
+func (server *Server) parseRequest(request *http.Request) (source *url.URL, parameters *Parameters, err error) {
+	if request.Method != "GET" {
+		err = errors.New("Invalid request method")
+		return
+	}
+
+	query := request.URL.Query()
+
+	if len(query["source"]) == 0 {
+		err = errors.New("Missing source parameter")
+		return
+	}
+	source, err = url.ParseRequestURI(query["source"][0])
+	if err != nil {
+		err = fmt.Errorf("Invalid source parameter (%s)", err)
+		return
+	}
+
+	parameters = &Parameters{}
+
+	if len(query["width"]) > 0 {
+		parameters.Width, err = strconv.Atoi(query["width"][0])
+		if err != nil {
+			return
+		}
+		if parameters.Width < 0 {
+			err = errors.New("Invalid width parameter")
+			return
+		}
+	}
+
+	if len(query["height"]) > 0 {
+		parameters.Height, err = strconv.Atoi(query["height"][0])
+		if err != nil {
+			return
+		}
+		if parameters.Height < 0 {
+			err = errors.New("Invalid height parameter")
+			return
+		}
+	}
+
+	return
 }
