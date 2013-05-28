70a1e4b6daba125da5f1e18b828b4f06a305ac87
Pierre Durand <pierre@plyce.com>
2013-05-28 23:46:09 +0200

    In memory cache

---
+++ cache/memory/memory.go
@@ -0,0 +1,50 @@
+package memory
+
+import (
+	"fmt"
+	"github.com/pierrre/imageproxy"
+	lru_impl "github.com/pierrre/imageproxy/cache/memory/lru"
+)
+
+type MemoryCache struct {
+	lru *lru_impl.LRUCache
+}
+
+func New(capacity uint64) *MemoryCache {
+	return &MemoryCache{
+		lru: lru_impl.NewLRUCache(capacity),
+	}
+}
+
+func (cache *MemoryCache) Get(key string) (image *imageproxy.Image, err error) {
+	hashedKey := imageproxy.HashCacheKey(key)
+	value, ok := cache.lru.Get(hashedKey)
+	if !ok {
+		err = fmt.Errorf("Image not found")
+		return
+	}
+	item, ok := value.(*item)
+	if !ok {
+		err = fmt.Errorf("The cache value is not an image")
+		return
+	}
+	image = item.image
+	return
+}
+
+func (cache *MemoryCache) Set(key string, image *imageproxy.Image) (err error) {
+	hashedKey := imageproxy.HashCacheKey(key)
+	item := &item{
+		image: image,
+	}
+	cache.lru.Set(hashedKey, item)
+	return
+}
+
+type item struct {
+	image *imageproxy.Image
+}
+
+func (item *item) Size() int {
+	return len(item.image.Data)
+}
--- example/main.go
+++ example/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"github.com/bradfitz/gomemcache/memcache"
+	//"github.com/bradfitz/gomemcache/memcache"
 	"github.com/pierrre/imageproxy"
-	imageproxy_cache_memcache "github.com/pierrre/imageproxy/cache/memcache"
+	//imageproxy_cache_memcache "github.com/pierrre/imageproxy/cache/memcache"
+	imageproxy_cache_memory "github.com/pierrre/imageproxy/cache/memory"
 	imageproxy_converter_graphicsmagick "github.com/pierrre/imageproxy/converter/graphicsmagick"
 	"net/http"
 )
@@ -14,10 +15,13 @@ func main() {
 			Addr: ":8080",
 		},
 		RequestParser: &imageproxy.SimpleRequestParser{},
-		Cache: &imageproxy_cache_memcache.MemcacheCache{
-			Prefix:   "imageproxy",
-			Memcache: memcache.New("localhost:11211"),
-		},
+		/*
+			Cache: &imageproxy_cache_memcache.MemcacheCache{
+				Prefix:   "imageproxy",
+				Memcache: memcache.New("localhost:11211"),
+			},
+		*/
+		Cache: imageproxy_cache_memory.New(10 * 1024 * 1024),
 		Converter: &imageproxy_converter_graphicsmagick.GraphicsMagickConverter{
 			Executable: "/usr/local/bin/gm",
 		},
