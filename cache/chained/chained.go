805763dfc5b556a96f0772ebd5db50ed942e438a
Pierre Durand <pierredurand@gmail.com>
2013-06-03 18:53:47 +0200

    Add chained cache

---
+++ cache/chained/chained.go
@@ -0,0 +1,40 @@
+package chained
+
+import (
+	"errors"
+	"github.com/pierrre/imageproxy"
+)
+
+type ChainedCache struct {
+	Caches []imageproxy.Cache
+}
+
+func (cache *ChainedCache) Get(key string) (*imageproxy.Image, error) {
+	for i, c := range cache.Caches {
+		image, err := c.Get(key)
+		if err == nil {
+			if i > 0 {
+				cache.setCaches(key, image, i)
+			}
+			return image, nil
+		}
+	}
+	return nil, errors.New("Image not found in chained cache")
+}
+
+func (cache *ChainedCache) setCaches(key string, image *imageproxy.Image, indexLimit int) {
+	for i := 0; i < indexLimit; i++ {
+		go func(i int) {
+			cache.Caches[i].Set(key, image)
+		}(i)
+	}
+}
+
+func (cache *ChainedCache) Set(key string, image *imageproxy.Image) (err error) {
+	for _, c := range cache.Caches {
+		go func(c imageproxy.Cache) {
+			c.Set(key, image)
+		}(c)
+	}
+	return
+}
--- example/main.go
+++ example/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	memcache_impl "github.com/bradfitz/gomemcache/memcache"
 	"github.com/pierrre/imageproxy"
+	imageproxy_cache_chained "github.com/pierrre/imageproxy/cache/chained"
+	imageproxy_cache_memcache "github.com/pierrre/imageproxy/cache/memcache"
 	imageproxy_cache_memory "github.com/pierrre/imageproxy/cache/memory"
 	imageproxy_cache_prefix "github.com/pierrre/imageproxy/cache/prefix"
 	imageproxy_converter_graphicsmagick "github.com/pierrre/imageproxy/converter/graphicsmagick"
@@ -10,7 +13,15 @@ import (
 )
 
 func main() {
-	cache := imageproxy_cache_memory.New(10 * 1024 * 1024)
+	cache := &imageproxy_cache_chained.ChainedCache{
+		Caches : []imageproxy.Cache{
+			imageproxy_cache_memory.New(10 * 1024 * 1024),
+			&imageproxy_cache_memcache.MemcacheCache{
+				Memcache: memcache_impl.New("localhost:11211"),
+			},
+		},
+	}
+
 
 	server := &imageproxy.Server{
 		HttpServer: &http.Server{
