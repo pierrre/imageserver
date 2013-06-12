83808d1a11806a1c8b0e5f201676b7c1dc40f512
Pierre Durand <pierredurand@gmail.com>
2013-06-12 15:17:39 +0200

    Add SourcePathParser

---
+++ http/parser/sourcepath/sourcepath.go
@@ -0,0 +1,23 @@
+package sourcepath
+
+import (
+	"github.com/pierrre/imageserver"
+	"net/http"
+	"net/url"
+)
+
+type SourcePathParser struct {
+	Base *url.URL
+}
+
+func (parser *SourcePathParser) Parse(request *http.Request) (parameters imageserver.Parameters, err error) {
+	s := *parser.Base
+	source := &s
+	source.Path += request.URL.Path
+
+	parameters = make(imageserver.Parameters)
+
+	parameters.Set("source", source.String())
+
+	return
+}
