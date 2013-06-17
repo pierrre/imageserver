5315871b36a00e730b22cd37778d32dd39c5cdc4
Pierre Durand <pierredurand@gmail.com>
2013-06-17 14:37:57 +0200

    rename http expires

--- http/server.go
+++ http/server.go
@@ -18,7 +18,7 @@ type Server struct {
 	Parser      Parser
 	ImageServer *imageserver.Server
 
-	ExpiresDuration time.Duration
+	Expires time.Duration
 }
 
 func (server *Server) Serve() {
@@ -78,9 +78,9 @@ func (server *Server) sendImage(writer http.ResponseWriter, image *imageserver.I
 
 	writer.Header().Set("Cache-Control", "public")
 
-	if server.ExpiresDuration != 0 {
+	if server.Expires != 0 {
 		t := time.Now()
-		t = t.Add(server.ExpiresDuration)
+		t = t.Add(server.Expires)
 		t = t.In(expiresHeaderLocation)
 		writer.Header().Set("Expires", t.Format(time.RFC1123))
 	}
