d144d22964ecd816b0fb902f0f95dff53ea34d3f
Pierre Durand <pierredurand@gmail.com>
2013-06-19 23:25:47 +0200

    Clean MemoryCache

--- cache/memory/memory.go
+++ cache/memory/memory.go
@@ -27,12 +27,12 @@ func (cache *MemoryCache) Get(key string, parameters imageserver.Parameters) (im
 	return
 }
 
-func (cache *MemoryCache) Set(key string, image *imageserver.Image, parameters imageserver.Parameters) (err error) {
+func (cache *MemoryCache) Set(key string, image *imageserver.Image, parameters imageserver.Parameters) error {
 	item := &item{
 		image: image,
 	}
 	cache.lru.Set(key, item)
-	return
+	return nil
 }
 
 type item struct {
