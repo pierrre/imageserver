aa7a7364f62e04fd547e464b95ff58494b6c5b42
Pierre Durand <pierredurand@gmail.com>
2013-06-20 00:02:42 +0200

    Clean CacheSource

--- source/cache/cache.go
+++ source/cache/cache.go
@@ -14,21 +14,15 @@ type CacheSource struct {
 
 func (source *CacheSource) Get(sourceId string, parameters imageserver.Parameters) (image *imageserver.Image, err error) {
 	cacheKey := source.getCacheKey(sourceId)
-
-	image, e := source.Cache.Get(cacheKey, parameters)
-	if e == nil {
+	if image, err = source.Cache.Get(cacheKey, parameters); err == nil {
 		return
 	}
-
-	image, err = source.Source.Get(sourceId, parameters)
-	if err != nil {
+	if image, err = source.Source.Get(sourceId, parameters); err != nil {
 		return
 	}
-
 	go func() {
 		_ = source.Cache.Set(cacheKey, image, parameters)
 	}()
-
 	return
 }
 
