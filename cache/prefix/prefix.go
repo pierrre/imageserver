bfbe4284c8663a0b184634bee8fb910419265d4d
Pierre Durand <pierredurand@gmail.com>
2013-06-21 18:40:45 +0200

    Clean PrefixCache

--- cache/prefix/prefix.go
+++ cache/prefix/prefix.go
@@ -9,10 +9,14 @@ type PrefixCache struct {
 	Cache  imageserver.Cache
 }
 
-func (cache *PrefixCache) Get(key string, parameters imageserver.Parameters) (*imageserver.Image, error) {
-	return cache.Cache.Get(cache.Prefix+key, parameters)
+func (cache *PrefixCache) Get(key string, parameters imageserver.Parameters) (image *imageserver.Image, err error) {
+	return cache.Cache.Get(cache.getKey(key), parameters)
 }
 
-func (cache *PrefixCache) Set(key string, image *imageserver.Image, parameters imageserver.Parameters) error {
-	return cache.Cache.Set(cache.Prefix+key, image, parameters)
+func (cache *PrefixCache) Set(key string, image *imageserver.Image, parameters imageserver.Parameters) (err error) {
+	return cache.Cache.Set(cache.getKey(key), image, parameters)
+}
+
+func (cache *PrefixCache) getKey(key string) string {
+	return cache.Prefix + key
 }
