e770264151954eb233740dd334a6c36a5ae63618
Pierre Durand <pierredurand@gmail.com>
2013-06-21 18:36:33 +0200

    clean ChainCache

--- cache/chain/chain.go
+++ cache/chain/chain.go
@@ -7,16 +7,18 @@ import (
 
 type ChainCache []imageserver.Cache
 
-func (cache ChainCache) Get(key string, parameters imageserver.Parameters) (*imageserver.Image, error) {
+func (cache ChainCache) Get(key string, parameters imageserver.Parameters) (image *imageserver.Image, err error) {
 	for i, c := range cache {
-		if image, err := c.Get(key, parameters); err == nil {
+		image, e := c.Get(key, parameters)
+		if e == nil {
 			if i > 0 {
 				cache.setCaches(key, image, parameters, i)
 			}
-			return image, nil
+			return
 		}
 	}
-	return nil, fmt.Errorf("Not found")
+	err = fmt.Errorf("Not found")
+	return
 }
 
 func (cache ChainCache) setCaches(key string, image *imageserver.Image, parameters imageserver.Parameters, indexLimit int) {
@@ -27,11 +29,11 @@ func (cache ChainCache) setCaches(key string, image *imageserver.Image, paramete
 	}
 }
 
-func (cache ChainCache) Set(key string, image *imageserver.Image, parameters imageserver.Parameters) error {
+func (cache ChainCache) Set(key string, image *imageserver.Image, parameters imageserver.Parameters) (err error) {
 	for _, c := range cache {
 		go func(c imageserver.Cache) {
 			c.Set(key, image, parameters)
 		}(c)
 	}
-	return nil
+	return
 }
