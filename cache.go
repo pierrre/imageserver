28e0000c17ed535c08a2110f34f67b83fe1bc4bd
Pierre Durand <pierredurand@gmail.com>
2013-08-26 17:40:33 +0200

    doc

--- cache.go
+++ cache.go
@@ -1,5 +1,6 @@
 package imageserver
 
+// Image cache
 type Cache interface {
 	Get(key string, parameters Parameters) (image *Image, err error)
 	Set(key string, image *Image, parameters Parameters) (err error)
