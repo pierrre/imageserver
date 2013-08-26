42d21c52f13ec7f8b1a7b30d5e217a794063b5e8
Pierre Durand <pierredurand@gmail.com>
2013-08-26 17:50:54 +0200

    doc

--- image.go
+++ image.go
@@ -5,11 +5,17 @@ import (
 	"encoding/gob"
 )
 
+// Internal image structure
+//
+// An image is composed of a type (png, jpeg, bmp, gif, ...) and data (slice of byte)
+//
+// This data structure is easy to serialize (cache) and manipulate (processor implementation)
 type Image struct {
 	Type string
 	Data []byte
 }
 
+// Serialize image to bytes
 func (image *Image) Marshal() (data []byte, err error) {
 	buffer := &bytes.Buffer{}
 	encoder := gob.NewEncoder(buffer)
@@ -20,6 +26,7 @@ func (image *Image) Marshal() (data []byte, err error) {
 	return
 }
 
+// Fill image with serialized bytes
 func (image *Image) Unmarshal(data []byte) (err error) {
 	buffer := bytes.NewBuffer(data)
 	decoder := gob.NewDecoder(buffer)
