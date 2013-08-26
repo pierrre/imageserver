9a650e214912c5b7f5789c16915732e22acc37e6
Pierre Durand <pierredurand@gmail.com>
2013-08-26 17:53:40 +0200

    doc

--- processor.go
+++ processor.go
@@ -1,5 +1,6 @@
 package imageserver
 
+// Processes an image and returns a new (or the same) image
 type Processor interface {
 	Process(inImage *Image, parameters Parameters) (image *Image, err error)
 }
