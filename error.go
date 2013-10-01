ac1893564886c07d2f1fbebe7787603e9653ae0b
Pierre Durand <pierredurand@gmail.com>
2013-10-01 13:55:16 +0200

    Add "Previous" field for error

--- error.go
+++ error.go
@@ -4,15 +4,22 @@ package imageserver
 //
 // It is used in the http server: the errors of this type are displayed, the others are shown as "internal error".
 type Error struct {
-	text string
+	Text     string
+	Previous error
 }
 
 func NewError(text string) *Error {
 	return &Error{
-		text: text,
+		Text: text,
 	}
 }
 
+func NewErrorWithPrevious(text string, previousErr error) *Error {
+	err := NewError(text)
+	err.Previous = previousErr
+	return err
+}
+
 func (err *Error) Error() string {
-	return err.text
+	return err.Text
 }
--- error_test.go
+++ error_test.go
@@ -1,13 +1,21 @@
 package imageserver
 
 import (
+	"errors"
 	"testing"
 )
 
 func TestError(t *testing.T) {
 	text := "foo"
-	err := NewError(text)
+	previous := errors.New("bar")
+	err := NewErrorWithPrevious(text, previous)
+	if err.Text != text {
+		t.Fatal("Invalid text")
+	}
+	if err.Previous != previous {
+		t.Fatal("Invalid previous")
+	}
 	if err.Error() != text {
-		t.FailNow()
+		t.Fatal("Invalid error message")
 	}
 }
