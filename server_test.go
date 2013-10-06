bd1d9d92e0b5f34fd25dedff88dc21b8e642098b
Pierre Durand <pierredurand@gmail.com>
2013-10-06 21:57:42 +0200

    test test test

--- server_test.go
+++ server_test.go
@@ -1,8 +1,39 @@
 package imageserver
 
 import (
+	"errors"
 	"testing"
 )
+
+type size struct {
+	width  int
+	height int
+}
+
+type providerSize struct{}
+
+func (provider *providerSize) Get(source interface{}, parameters Parameters) (*Image, error) {
+	size, ok := source.(size)
+	if !ok {
+		return nil, errors.New("Source is not a size")
+	}
+	return CreateImage(size.width, size.height), nil
+}
+
+func TestServerGetSource(t *testing.T) {
+	server := &Server{
+		Provider: new(providerSize),
+	}
+	_, err := server.getSource(Parameters{
+		"source": size{
+			width:  500,
+			height: 400,
+		},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
 
 func TestServerGetSourceErrorMissingSource(t *testing.T) {
 	server := &Server{}
