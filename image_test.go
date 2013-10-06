ca5542025573957b55cfdb5c5ffe5d2446088505
Pierre Durand <pierredurand@gmail.com>
2013-10-06 20:36:54 +0200

    simplify image test

--- image_test.go
+++ image_test.go
@@ -10,52 +10,39 @@ import (
 )
 
 func TestImage(t *testing.T) {
-	i1 := CreateBaseImage(500, 400)
-	buffer1 := new(bytes.Buffer)
-	png.Encode(buffer1, i1)
-	im1 := &Image{
-		Type: "png",
-		Data: buffer1.Bytes(),
-	}
-	data, err := im1.Marshal()
+	image1 := CreateImage(500, 400)
+	data, err := image1.Marshal()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	im2 := new(Image)
-	err = im2.Unmarshal(data)
+	image2 := new(Image)
+	err = image2.Unmarshal(data)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if im2.Type != im1.Type {
-		t.Fatalf("Types not equals: %s %s", im1.Type, im2.Type)
+
+	if image2.Type != image1.Type {
+		t.Fatalf("Types not equals: %s %s", image1.Type, image2.Type)
 	}
-	if len(im2.Data) != len(im1.Data) {
-		t.Fatalf("Data not the same size %d %d", len(im1.Data), len(im2.Data))
+	if len(image2.Data) != len(image1.Data) {
+		t.Fatalf("Data not the same size %d %d", len(image1.Data), len(image2.Data))
 	}
-	for i, b := range im2.Data {
-		if b != im1.Data[i] {
-			t.Fatalf("Data not equals at index %d: %d %d", i, im1.Data[i], b)
+	for i, b2 := range image2.Data {
+		b1 := image1.Data[i]
+		if b2 != b1 {
+			t.Fatalf("Data not equals at index %d: %d %d", i, b1, b2)
 		}
 	}
-	buffer2 := bytes.NewBuffer(im2.Data)
-	i2, err := png.Decode(buffer2)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !i2.Bounds().Eq(i1.Bounds()) {
-		t.Fatal("Image bounds not equals %s %s", i1.Bounds(), i2.Bounds())
-	}
-	for y, height := 0, i2.Bounds().Dy(); y < height; y++ {
-		for x, width := 0, i2.Bounds().Dx(); x < width; x++ {
-			c1 := i1.At(x, y)
-			r1, g1, b1, a1 := c1.RGBA()
-			c2 := i2.At(x, y)
-			r2, g2, b2, a2 := i2.At(x, y).RGBA()
-			if r1 != r2 || g1 != g2 || b1 != b2 || a1 != a2 {
-				t.Fatalf("Colors not equals at %d,%d: %s %s", x, y, c1, c2)
-			}
-		}
+}
+
+func CreateImage(width, height int) *Image {
+	baseImage := CreateBaseImage(width, height)
+	buffer := new(bytes.Buffer)
+	png.Encode(buffer, baseImage)
+	return &Image{
+		Type: "png",
+		Data: buffer.Bytes(),
 	}
 }
 
