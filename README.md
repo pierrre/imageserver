2ea7c7117bbf0adbca651f9311b961318a057c90
Pierre <pierredurand@gmail.com>
2013-12-02 18:26:04 +0100

    Improve GraphicsMagick benchmark

--- README.md
+++ README.md
@@ -28,6 +28,34 @@ http://fuckingfrogs.fr:8080/?source=https://www.google.com/images/srpr/logo6w.pn
 ```
 ![Extent background](http://fuckingfrogs.fr:8080/?source=https://www.google.com/images/srpr/logo6w.png&width=200&height=200&extent=1&background=000000)
 
+## Resize benchmark
+```
+go test -bench=. -benchtime=10s ./processor/graphicsmagick
+testing: warning: no tests to run
+PASS
+BenchmarkResizeSmallWorker1-8	    5000	   8188875 ns/op
+BenchmarkResizeMediumWorker1-8	    1000	  23143921 ns/op
+BenchmarkResizeLargeWorker1-8	     500	  52679041 ns/op
+BenchmarkResizeHugeWorker1-8	      50	 389060935 ns/op
+BenchmarkResizeAnimatedWorker1-8	      50	 311136430 ns/op
+BenchmarkResizeSmallWorker2-8	    5000	   5786665 ns/op
+BenchmarkResizeMediumWorker2-8	    1000	  16159015 ns/op
+BenchmarkResizeLargeWorker2-8	     500	  36070204 ns/op
+BenchmarkResizeHugeWorker2-8	      50	 247750150 ns/op
+BenchmarkResizeAnimatedWorker2-8	      50	 221887473 ns/op
+BenchmarkResizeSmallWorker4-8	    5000	   5674571 ns/op
+BenchmarkResizeMediumWorker4-8	    2000	  13427690 ns/op
+BenchmarkResizeLargeWorker4-8	    1000	  29846192 ns/op
+BenchmarkResizeHugeWorker4-8	      50	 217280287 ns/op
+BenchmarkResizeAnimatedWorker4-8	      50	 243820932 ns/op
+BenchmarkResizeSmallWorker8-8	    5000	   4770737 ns/op
+BenchmarkResizeMediumWorker8-8	    2000	  12349114 ns/op
+BenchmarkResizeLargeWorker8-8	    1000	  27195685 ns/op
+BenchmarkResizeHugeWorker8-8	      50	 214468274 ns/op
+BenchmarkResizeAnimatedWorker8-8	      50	 200378466 ns/op
+ok  	github.com/pierrre/imageserver/processor/graphicsmagick	443.701s
+```
+
 ## Status
 [![Build Status](https://travis-ci.org/pierrre/imageserver.png?branch=master)](https://travis-ci.org/pierrre/imageserver)
 
--- processor/graphicsmagick/graphicsmagick_test.go
+++ processor/graphicsmagick/graphicsmagick_test.go
@@ -3,44 +3,130 @@ package graphicsmagick
 import (
 	"github.com/pierrre/imageserver"
 	"github.com/pierrre/imageserver/testdata"
+	"sync"
 	"testing"
 )
 
-func BenchmarkResizeSmall(b *testing.B) {
-	benchmarkResize(b, testdata.Small)
+// Worker 1
+
+func BenchmarkResizeSmallWorker1(b *testing.B) {
+	benchmarkResize(b, testdata.Small, 1)
 }
 
-func BenchmarkResizeMedium(b *testing.B) {
-	benchmarkResize(b, testdata.Medium)
+func BenchmarkResizeMediumWorker1(b *testing.B) {
+	benchmarkResize(b, testdata.Medium, 1)
 }
 
-func BenchmarkResizeLarge(b *testing.B) {
-	benchmarkResize(b, testdata.Large)
+func BenchmarkResizeLargeWorker1(b *testing.B) {
+	benchmarkResize(b, testdata.Large, 1)
 }
 
-func BenchmarkResizeHuge(b *testing.B) {
-	benchmarkResize(b, testdata.Huge)
+func BenchmarkResizeHugeWorker1(b *testing.B) {
+	benchmarkResize(b, testdata.Huge, 1)
 }
 
-func BenchmarkResizeAnimated(b *testing.B) {
-	benchmarkResize(b, testdata.Animated)
+func BenchmarkResizeAnimatedWorker1(b *testing.B) {
+	benchmarkResize(b, testdata.Animated, 1)
 }
 
-func benchmarkResize(b *testing.B, image *imageserver.Image) {
+// Worker 2
+
+func BenchmarkResizeSmallWorker2(b *testing.B) {
+	benchmarkResize(b, testdata.Small, 2)
+}
+
+func BenchmarkResizeMediumWorker2(b *testing.B) {
+	benchmarkResize(b, testdata.Medium, 2)
+}
+
+func BenchmarkResizeLargeWorker2(b *testing.B) {
+	benchmarkResize(b, testdata.Large, 2)
+}
+
+func BenchmarkResizeHugeWorker2(b *testing.B) {
+	benchmarkResize(b, testdata.Huge, 2)
+}
+
+func BenchmarkResizeAnimatedWorker2(b *testing.B) {
+	benchmarkResize(b, testdata.Animated, 2)
+}
+
+// Worker 4
+
+func BenchmarkResizeSmallWorker4(b *testing.B) {
+	benchmarkResize(b, testdata.Small, 4)
+}
+
+func BenchmarkResizeMediumWorker4(b *testing.B) {
+	benchmarkResize(b, testdata.Medium, 4)
+}
+
+func BenchmarkResizeLargeWorker4(b *testing.B) {
+	benchmarkResize(b, testdata.Large, 4)
+}
+
+func BenchmarkResizeHugeWorker4(b *testing.B) {
+	benchmarkResize(b, testdata.Huge, 4)
+}
+
+func BenchmarkResizeAnimatedWorker4(b *testing.B) {
+	benchmarkResize(b, testdata.Animated, 4)
+}
+
+// Worker 8
+
+func BenchmarkResizeSmallWorker8(b *testing.B) {
+	benchmarkResize(b, testdata.Small, 8)
+}
+
+func BenchmarkResizeMediumWorker8(b *testing.B) {
+	benchmarkResize(b, testdata.Medium, 8)
+}
+
+func BenchmarkResizeLargeWorker8(b *testing.B) {
+	benchmarkResize(b, testdata.Large, 8)
+}
+
+func BenchmarkResizeHugeWorker8(b *testing.B) {
+	benchmarkResize(b, testdata.Huge, 8)
+}
+
+func BenchmarkResizeAnimatedWorker8(b *testing.B) {
+	benchmarkResize(b, testdata.Animated, 8)
+}
+
+func benchmarkResize(b *testing.B, image *imageserver.Image, workerCount int) {
 	parameters := imageserver.Parameters{
 		"graphicsmagick": imageserver.Parameters{
 			"width":  100,
 			"height": 100,
 		},
 	}
+
 	processor := &GraphicsMagickProcessor{
 		Executable: "gm",
 	}
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_, err := processor.Process(image, parameters)
-		if err != nil {
-			b.Fatal(err)
+
+	work := make(chan bool)
+	go func() {
+		for i := 0; i < b.N; i++ {
+			work <- true
 		}
+		close(work)
+	}()
+
+	wg := new(sync.WaitGroup)
+	wg.Add(workerCount)
+	for i := 0; i < workerCount; i++ {
+		go func() {
+			for _ = range work {
+				_, err := processor.Process(image, parameters)
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+			wg.Done()
+		}()
 	}
+	wg.Wait()
 }
