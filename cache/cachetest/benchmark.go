5f00d613806c81419f3b3d1cb1b921a3c7be583a
Pierre Durand <pierredurand@gmail.com>
2013-12-28 19:53:47 +0100

    refactor RedisCache benchmark

---
+++ cache/cachetest/benchmark.go
@@ -0,0 +1,43 @@
+package cachetest
+
+import (
+	"github.com/pierrre/imageserver"
+	"sync"
+	"testing"
+)
+
+// CacheBenchmarkGet is a helper to benchmark cache Get()
+func CacheBenchmarkGet(b *testing.B, cache imageserver.Cache, workerCount int, image *imageserver.Image) {
+	key := "test"
+	parameters := make(imageserver.Parameters)
+	err := cache.Set(key, image, parameters)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+
+	work := make(chan bool)
+	go func() {
+		for i := 0; i < b.N; i++ {
+			work <- true
+		}
+		close(work)
+	}()
+	wg := new(sync.WaitGroup)
+	wg.Add(workerCount)
+	for i := 0; i < workerCount; i++ {
+		go func() {
+			for _ = range work {
+				_, err := cache.Get(key, parameters)
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	b.SetBytes(int64(len(image.Data)))
+}
--- cache/redis/benchmark_test.go
+++ cache/redis/benchmark_test.go
@@ -2,155 +2,144 @@ package redis
 
 import (
 	"github.com/pierrre/imageserver"
+	"github.com/pierrre/imageserver/cache/cachetest"
 	"github.com/pierrre/imageserver/testdata"
-	"sync"
 	"testing"
 )
 
 // Worker 1
 
-func BenchmarkGetSmallWorker1(b *testing.B) {
-	benchmarkGet(b, testdata.Small, 1)
+func BenchmarkGetWorker1Small(b *testing.B) {
+	benchmarkGetWorker1(b, testdata.Small)
 }
 
-func BenchmarkGetMediumWorker1(b *testing.B) {
-	benchmarkGet(b, testdata.Medium, 1)
+func BenchmarkGetWorker1Medium(b *testing.B) {
+	benchmarkGetWorker1(b, testdata.Medium)
 }
 
-func BenchmarkGetLargeWorker1(b *testing.B) {
-	benchmarkGet(b, testdata.Large, 1)
+func BenchmarkGetWorker1Large(b *testing.B) {
+	benchmarkGetWorker1(b, testdata.Large)
 }
 
-func BenchmarkGetHugeWorker1(b *testing.B) {
-	benchmarkGet(b, testdata.Huge, 1)
+func BenchmarkGetWorker1Huge(b *testing.B) {
+	benchmarkGetWorker1(b, testdata.Huge)
 }
 
-func BenchmarkGetAnimatedWorker1(b *testing.B) {
-	benchmarkGet(b, testdata.Animated, 1)
+func BenchmarkGetWorker1Animated(b *testing.B) {
+	benchmarkGetWorker1(b, testdata.Animated)
+}
+
+func benchmarkGetWorker1(b *testing.B, image *imageserver.Image) {
+	benchmarkGet(b, 1, image)
 }
 
 // Worker 2
 
-func BenchmarkGetSmallWorker2(b *testing.B) {
-	benchmarkGet(b, testdata.Small, 2)
+func BenchmarkGetWorker2Small(b *testing.B) {
+	benchmarkGetWorker2(b, testdata.Small)
 }
 
-func BenchmarkGetMediumWorker2(b *testing.B) {
-	benchmarkGet(b, testdata.Medium, 2)
+func BenchmarkGetWorker2Medium(b *testing.B) {
+	benchmarkGetWorker2(b, testdata.Medium)
 }
 
-func BenchmarkGetLargeWorker2(b *testing.B) {
-	benchmarkGet(b, testdata.Large, 2)
+func BenchmarkGetWorker2Large(b *testing.B) {
+	benchmarkGetWorker2(b, testdata.Large)
 }
 
-func BenchmarkGetHugeWorker2(b *testing.B) {
-	benchmarkGet(b, testdata.Huge, 2)
+func BenchmarkGetWorker2Huge(b *testing.B) {
+	benchmarkGetWorker2(b, testdata.Huge)
 }
 
-func BenchmarkGetAnimatedWorker2(b *testing.B) {
-	benchmarkGet(b, testdata.Animated, 2)
+func BenchmarkGetWorker2Animated(b *testing.B) {
+	benchmarkGetWorker2(b, testdata.Animated)
+}
+
+func benchmarkGetWorker2(b *testing.B, image *imageserver.Image) {
+	benchmarkGet(b, 2, image)
 }
 
 // Worker 4
 
-func BenchmarkGetSmallWorker4(b *testing.B) {
-	benchmarkGet(b, testdata.Small, 4)
+func BenchmarkGetWorker4Small(b *testing.B) {
+	benchmarkGetWorker4(b, testdata.Small)
 }
 
-func BenchmarkGetMediumWorker4(b *testing.B) {
-	benchmarkGet(b, testdata.Medium, 4)
+func BenchmarkGetWorker4Medium(b *testing.B) {
+	benchmarkGetWorker4(b, testdata.Medium)
 }
 
-func BenchmarkGetLargeWorker4(b *testing.B) {
-	benchmarkGet(b, testdata.Large, 4)
+func BenchmarkGetWorker4Large(b *testing.B) {
+	benchmarkGetWorker4(b, testdata.Large)
 }
 
-func BenchmarkGetHugeWorker4(b *testing.B) {
-	benchmarkGet(b, testdata.Huge, 4)
+func BenchmarkGetWorker4Huge(b *testing.B) {
+	benchmarkGetWorker4(b, testdata.Huge)
 }
 
-func BenchmarkGetAnimatedWorker4(b *testing.B) {
-	benchmarkGet(b, testdata.Animated, 4)
+func BenchmarkGetWorker4Animated(b *testing.B) {
+	benchmarkGetWorker4(b, testdata.Animated)
+}
+
+func benchmarkGetWorker4(b *testing.B, image *imageserver.Image) {
+	benchmarkGet(b, 4, image)
 }
 
 // Worker 8
 
-func BenchmarkGetSmallWorker8(b *testing.B) {
-	benchmarkGet(b, testdata.Small, 8)
+func BenchmarkGetWorker8Small(b *testing.B) {
+	benchmarkGetWorker8(b, testdata.Small)
 }
 
-func BenchmarkGetMediumWorker8(b *testing.B) {
-	benchmarkGet(b, testdata.Medium, 8)
+func BenchmarkGetWorker8Medium(b *testing.B) {
+	benchmarkGetWorker8(b, testdata.Medium)
 }
 
-func BenchmarkGetLargeWorker8(b *testing.B) {
-	benchmarkGet(b, testdata.Large, 8)
+func BenchmarkGetWorker8Large(b *testing.B) {
+	benchmarkGetWorker8(b, testdata.Large)
 }
 
-func BenchmarkGetHugeWorker8(b *testing.B) {
-	benchmarkGet(b, testdata.Huge, 8)
+func BenchmarkGetWorker8Huge(b *testing.B) {
+	benchmarkGetWorker8(b, testdata.Huge)
 }
 
-func BenchmarkGetAnimatedWorker8(b *testing.B) {
-	benchmarkGet(b, testdata.Animated, 8)
+func BenchmarkGetWorker8Animated(b *testing.B) {
+	benchmarkGetWorker8(b, testdata.Animated)
+}
+
+func benchmarkGetWorker8(b *testing.B, image *imageserver.Image) {
+	benchmarkGet(b, 8, image)
 }
 
 // Worker 16
 
-func BenchmarkGetSmallWorker16(b *testing.B) {
-	benchmarkGet(b, testdata.Small, 16)
+func BenchmarkGetWorker16Small(b *testing.B) {
+	benchmarkGetWorker16(b, testdata.Small)
 }
 
-func BenchmarkGetMediumWorker16(b *testing.B) {
-	benchmarkGet(b, testdata.Medium, 16)
+func BenchmarkGetWorker16Medium(b *testing.B) {
+	benchmarkGetWorker16(b, testdata.Medium)
 }
 
-func BenchmarkGetLargeWorker16(b *testing.B) {
-	benchmarkGet(b, testdata.Large, 16)
+func BenchmarkGetWorker16Large(b *testing.B) {
+	benchmarkGetWorker16(b, testdata.Large)
 }
 
-func BenchmarkGetHugeWorker16(b *testing.B) {
-	benchmarkGet(b, testdata.Huge, 16)
+func BenchmarkGetWorker16Huge(b *testing.B) {
+	benchmarkGetWorker16(b, testdata.Huge)
 }
 
-func BenchmarkGetAnimatedWorker16(b *testing.B) {
-	benchmarkGet(b, testdata.Animated, 16)
+func BenchmarkGetWorker16Animated(b *testing.B) {
+	benchmarkGetWorker16(b, testdata.Animated)
 }
 
-func benchmarkGet(b *testing.B, image *imageserver.Image, workerCount int) {
+func benchmarkGetWorker16(b *testing.B, image *imageserver.Image) {
+	benchmarkGet(b, 16, image)
+}
+
+func benchmarkGet(b *testing.B, workerCount int, image *imageserver.Image) {
 	cache := createTestCache()
 	defer cache.Close()
 
-	key := "test"
-	parameters := make(imageserver.Parameters)
-	err := cache.Set(key, image, parameters)
-	if err != nil {
-		b.Fatal(err)
-	}
-
-	b.ResetTimer()
-
-	work := make(chan bool)
-	go func() {
-		for i := 0; i < b.N; i++ {
-			work <- true
-		}
-		close(work)
-	}()
-	wg := new(sync.WaitGroup)
-	wg.Add(workerCount)
-	for i := 0; i < workerCount; i++ {
-		go func() {
-			for _ = range work {
-				_, err := cache.Get(key, parameters)
-				if err != nil {
-					b.Fatal(err)
-				}
-			}
-			wg.Done()
-		}()
-	}
-	wg.Wait()
-
-	b.SetBytes(int64(len(image.Data)))
+	cachetest.CacheBenchmarkGet(b, cache, workerCount, image)
 }
