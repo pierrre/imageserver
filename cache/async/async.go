712583b039db61b48359997f9b83cd36f5027e59
Pierre Durand <pierredurand@gmail.com>
2014-01-05 20:27:04 +0100

    add AsyncCache

---
+++ cache/async/async.go
@@ -0,0 +1,24 @@
+// Package async provides an asynchronous cache
+package async
+
+import (
+	"github.com/pierrre/imageserver"
+)
+
+// AsyncCache represent an asynchronous cache
+type AsyncCache struct {
+	imageserver.Cache
+	ErrFunc func(err error, key string, image *imageserver.Image, parameters imageserver.Parameters)
+}
+
+// Set sets the Image to the underlying Cache using another goroutine
+func (cache *AsyncCache) Set(key string, image *imageserver.Image, parameters imageserver.Parameters) error {
+	go func() {
+		err := cache.Cache.Set(key, image, parameters)
+		if err != nil && cache.ErrFunc != nil {
+			cache.ErrFunc(err, key, image, parameters)
+		}
+	}()
+
+	return nil
+}
---
+++ cache/async/async_test.go
@@ -0,0 +1,3 @@
+package async
+
+//TODO
