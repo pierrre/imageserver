ad290ba71092b77ce24fec78f2f49f2464e0d741
Pierre Durand <pierredurand@gmail.com>
2014-01-05 21:01:28 +0100

    remove async feature from ListCache

--- cache/list/list.go
+++ cache/list/list.go
@@ -14,10 +14,12 @@ type ListCache []imageserver.Cache
 func (cache ListCache) Get(key string, parameters imageserver.Parameters) (*imageserver.Image, error) {
 	for i, c := range cache {
 		image, err := c.Get(key, parameters)
-
 		if err == nil {
 			if i > 0 {
-				cache.setCaches(key, image, parameters, i)
+				err = cache.set(key, image, parameters, i)
+				if err != nil {
+					return nil, err
+				}
 			}
 			return image, nil
 		}
@@ -26,20 +28,17 @@ func (cache ListCache) Get(key string, parameters imageserver.Parameters) (*imag
 	return nil, imageserver.NewCacheMissError(key, cache, nil)
 }
 
-func (cache ListCache) setCaches(key string, image *imageserver.Image, parameters imageserver.Parameters, indexLimit int) {
+func (cache ListCache) set(key string, image *imageserver.Image, parameters imageserver.Parameters, indexLimit int) error {
 	for i := 0; i < indexLimit; i++ {
-		go func(i int) {
-			cache[i].Set(key, image, parameters)
-		}(i)
+		err := cache[i].Set(key, image, parameters)
+		if err != nil {
+			return err
+		}
 	}
+	return nil
 }
 
 // Set sets the image to all caches
 func (cache ListCache) Set(key string, image *imageserver.Image, parameters imageserver.Parameters) error {
-	for _, c := range cache {
-		go func(c imageserver.Cache) {
-			c.Set(key, image, parameters)
-		}(c)
-	}
-	return nil
+	return cache.set(key, image, parameters, len(cache))
 }
