a2353056e7eb43b89cfd1ccd479b73257dadd8ae
Pierre Durand <pierredurand@gmail.com>
2014-01-18 19:00:22 +0100

    add tests for MemoryCache

--- cache/memory/memory_test.go
+++ cache/memory/memory_test.go
@@ -1,5 +1,22 @@
 package memory
 
+import (
+	"github.com/pierrre/imageserver/cache/cachetest"
+	"testing"
+)
+
+func TestGetSet(t *testing.T) {
+	cache := createTestCache()
+
+	cachetest.CacheTestGetSetAllImages(t, cache)
+}
+
+func TestGetErrorMiss(t *testing.T) {
+	cache := createTestCache()
+
+	cachetest.CacheTestGetErrorMiss(t, cache)
+}
+
 func createTestCache() *MemoryCache {
 	return New(20 * 1024 * 1024)
 }
