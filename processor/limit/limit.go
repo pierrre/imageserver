238d46b601b512237dac4f0d1ab75e9a6779325d
Pierre Durand <pierredurand@gmail.com>
2014-03-27 18:30:24 +0100

    use struct{} instead of bool in LimitProcessor

--- processor/limit/limit.go
+++ processor/limit/limit.go
@@ -10,20 +10,20 @@ import (
 // It wraps an Image Processor and use a buffered channel to limit the number of concurrent executions.
 type LimitProcessor struct {
 	imageserver.Processor
-	LimitChan chan bool
+	LimitChan chan struct{}
 }
 
 // New creates a LimitProcessor
 func New(processor imageserver.Processor, limit uint) imageserver.Processor {
 	return &LimitProcessor{
 		Processor: processor,
-		LimitChan: make(chan bool, limit),
+		LimitChan: make(chan struct{}, limit),
 	}
 }
 
 // Process forwards the call to the wrapped Image Processor and limits the number of concurrent executions
 func (processor *LimitProcessor) Process(image *imageserver.Image, parameters imageserver.Parameters) (*imageserver.Image, error) {
-	processor.LimitChan <- true
+	processor.LimitChan <- struct{}{}
 	defer func() {
 		<-processor.LimitChan
 	}()
