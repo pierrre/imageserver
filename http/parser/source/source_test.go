a56cf8ad2d78a97f9b4914b0cd888f87f434cee6
Pierre Durand <pierredurand@gmail.com>
2014-05-16 16:22:50 +0200

    add test for SourceParser

--- http/parser/source/source_test.go
+++ http/parser/source/source_test.go
@@ -1,3 +1,32 @@
 package source
 
-//TODO
+import (
+	"net/http"
+	"testing"
+
+	"github.com/pierrre/imageserver"
+)
+
+func TestParse(t *testing.T) {
+	parser := &SourceParser{}
+
+	request, err := http.NewRequest("GET", "http://localhost?source=foo", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	parameters := make(imageserver.Parameters)
+
+	err = parser.Parse(request, parameters)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	v, err := parameters.GetString("source")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if v != "foo" {
+		t.Fatal("Wrong value")
+	}
+}
