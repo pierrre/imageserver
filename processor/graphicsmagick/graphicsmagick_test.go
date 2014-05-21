35dd0bfbed1fbcc31d04b52128fa89ebd47efb84
Pierre Durand <pierredurand@gmail.com>
2014-05-21 16:08:26 +0200

    add GraphicsMagickProcessor tests (WIP)

--- processor/graphicsmagick/graphicsmagick_test.go
+++ processor/graphicsmagick/graphicsmagick_test.go
@@ -1,3 +1,50 @@
 package graphicsmagick
 
-//TODO
+import (
+	"testing"
+	"time"
+
+	"github.com/pierrre/imageserver"
+	"github.com/pierrre/imageserver/testdata"
+)
+
+func TestProcess(t *testing.T) {
+	image := testdata.Medium
+
+	parameters := imageserver.Parameters{
+		"graphicsmagick": imageserver.Parameters{
+			"width":  100,
+			"height": 100,
+		},
+	}
+
+	processor := &GraphicsMagickProcessor{
+		Executable: "gm",
+	}
+
+	_, err := processor.Process(image, parameters)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestProcessErrorTimeout(t *testing.T) {
+	image := testdata.Medium
+
+	parameters := imageserver.Parameters{
+		"graphicsmagick": imageserver.Parameters{
+			"width":  100,
+			"height": 100,
+		},
+	}
+
+	processor := &GraphicsMagickProcessor{
+		Executable: "gm",
+		Timeout:    1 * time.Nanosecond,
+	}
+
+	_, err := processor.Process(image, parameters)
+	if err == nil {
+		t.Fatal("no error")
+	}
+}
