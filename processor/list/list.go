aeee784ef63c605024b56c0e75ddbbf94be0bb87
Pierre Durand <pierredurand@gmail.com>
2014-05-21 12:18:15 +0200

    add ListProcessor

---
+++ processor/list/list.go
@@ -0,0 +1,21 @@
+// Package list provides a list of Image Processor
+package list
+
+import (
+	"github.com/pierrre/imageserver"
+)
+
+// ListProcessor represents a list of Image Processor
+type ListProcessor []imageserver.Processor
+
+// Process processes the Image with the list of Image Processor
+func (processor ListProcessor) Process(image *imageserver.Image, parameters imageserver.Parameters) (*imageserver.Image, error) {
+	var err error
+	for _, p := range processor {
+		image, err = p.Process(image, parameters)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return image, nil
+}
