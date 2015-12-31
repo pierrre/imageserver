6eb83663db8258c9837629a0cf8be78a994e5f6e
Pierre Durand <pierredurand@gmail.com>
2015-12-31 19:54:34 +0100

    move functions in image/internal

--- image/internal/util.go
+++ image/internal/util.go
@@ -8,6 +8,34 @@ import (
 	"sync"
 )
 
+// RGBAToNRGBA converts RGBA to NRGBA.
+func RGBAToNRGBA(r, g, b, a uint32) (uint32, uint32, uint32, uint32) {
+	if a == 0xffff {
+		return r, g, b, a
+	}
+	if a == 0 {
+		return 0, 0, 0, 0
+	}
+	r = r * 0xffff / a
+	g = g * 0xffff / a
+	b = b * 0xffff / a
+	return r, g, b, a
+}
+
+// NRGBAToRGBA converts NRGBA to RGBA.
+func NRGBAToRGBA(r, g, b, a uint32) (uint32, uint32, uint32, uint32) {
+	if a == 0xffff {
+		return r, g, b, a
+	}
+	if a == 0 {
+		return 0, 0, 0, 0
+	}
+	r = r * a / 0xffff
+	g = g * a / 0xffff
+	b = b * a / 0xffff
+	return r, g, b, a
+}
+
 // NewDrawable returns a new draw.Image with the same type and size as p.
 // If p has no size, 1x1 is used.
 // See NewDrawableSize.
@@ -65,16 +93,6 @@ func Copy(dst draw.Image, src image.Image) {
 	})
 }
 
-func sqDiff(x, y uint32) uint32 {
-	var d uint32
-	if x > y {
-		d = x - y
-	} else {
-		d = y - x
-	}
-	return (d * d) >> 2
-}
-
 // Parallel helps to dispatch tasks concurrently.
 // It calls f with arguments (0,a) (a,b) ... (x,n).
 // Currently, it starts GOMAXPROCS goroutines.
@@ -109,4 +127,43 @@ func parallel(n int, p int, f func(start, end int)) {
 		}(i)
 	}
 	wg.Wait()
+}
+
+type colorRGBA struct {
+	r, g, b, a uint32
+}
+
+type paletteRGBA []colorRGBA
+
+func newPaletteRGBA(pl color.Palette) paletteRGBA {
+	pa := make(paletteRGBA, len(pl))
+	for i, c := range pl {
+		r, g, b, a := c.RGBA()
+		pa[i] = colorRGBA{r, g, b, a}
+	}
+	return pa
+}
+
+func (pa paletteRGBA) index(c colorRGBA) int {
+	ret, bestSum := 0, uint32(1<<32-1)
+	for i, ca := range pa {
+		sum := sqDiff(c.r, ca.r) + sqDiff(c.g, ca.g) + sqDiff(c.b, ca.b) + sqDiff(c.a, ca.a)
+		if sum < bestSum {
+			if sum == 0 {
+				return i
+			}
+			ret, bestSum = i, sum
+		}
+	}
+	return ret
+}
+
+func sqDiff(x, y uint32) uint32 {
+	var d uint32
+	if x > y {
+		d = x - y
+	} else {
+		d = y - x
+	}
+	return (d * d) >> 2
 }
--- image/internal/util_benchmark_test.go
+++ image/internal/util_benchmark_test.go
@@ -5,6 +5,54 @@ import (
 	"testing"
 )
 
+func BenchmarkRGBAToNRGBAOpaque(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			RGBAToNRGBA(0xffff, 0x8000, 0x0000, 0xffff)
+		}
+	})
+}
+
+func BenchmarkRGBAToNRGBATransparent(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			RGBAToNRGBA(0x0000, 0x0000, 0x0000, 0x0000)
+		}
+	})
+}
+
+func BenchmarkRGBAToNRGBATranslucent(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			RGBAToNRGBA(0x8000, 0x4000, 0x0000, 0x8000)
+		}
+	})
+}
+
+func BenchmarkNRGBAToRGBAOpaque(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			NRGBAToRGBA(0xffff, 0x8000, 0x0000, 0xffff)
+		}
+	})
+}
+
+func BenchmarkNRGBAToRGBATransparent(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			NRGBAToRGBA(0xffff, 0x8000, 0x0000, 0x0000)
+		}
+	})
+}
+
+func BenchmarkNRGBAToRGBATranslucent(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			NRGBAToRGBA(0xffff, 0x8000, 0x0000, 0x8000)
+		}
+	})
+}
+
 func BenchmarkCopy(b *testing.B) {
 	src := image.NewRGBA(image.Rect(0, 0, 100, 100))
 	testDrawRandom(src)
