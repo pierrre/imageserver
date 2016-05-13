50f6ca05c128cd749cbb27ebc1b5cf71ecc8ee14
Pierre Durand <pierredurand@gmail.com>
2016-05-13 14:29:38 +0200

    simplify advanced example & add groupcache example

--- examples/advanced/advanced.go
+++ examples/advanced/advanced.go
@@ -3,23 +3,16 @@ package main
 
 import (
 	"crypto/sha256"
-	"encoding/json"
 	"flag"
-	"fmt"
-	"log"
 	"net/http"
-	"net/url"
 	"os"
 	"runtime"
-	"strings"
 	"time"
 
 	"github.com/disintegration/gift"
-	"github.com/golang/groupcache"
 	"github.com/pierrre/githubhook"
 	"github.com/pierrre/imageserver"
 	imageserver_cache "github.com/pierrre/imageserver/cache"
-	imageserver_cache_groupcache "github.com/pierrre/imageserver/cache/groupcache"
 	imageserver_cache_memory "github.com/pierrre/imageserver/cache/memory"
 	imageserver_http "github.com/pierrre/imageserver/http"
 	imageserver_http_crop "github.com/pierrre/imageserver/http/crop"
@@ -38,53 +31,28 @@ import (
 	imageserver_testdata "github.com/pierrre/imageserver/testdata"
 )
 
-const (
-	groupcacheName = "imageserver"
-)
-
 var (
 	flagHTTPAddr            = ":8080"
 	flagGitHubWebhookSecret string
-	flagGroupcache          = int64(0)
-	flagGroupcacheSelf      string
-	flagGroupcachePeers     string
-	flagCacheMemory         = int64(128 * (1 << 20))
+	flagCache               = int64(128 * (1 << 20))
 )
 
 func main() {
 	parseFlags()
-	initLog()
-	logEnv()
-	startGroupcacheHTTPServer()
 	startHTTPServer()
 }
 
 func parseFlags() {
 	flag.StringVar(&flagHTTPAddr, "http", flagHTTPAddr, "HTTP addr")
 	flag.StringVar(&flagGitHubWebhookSecret, "github-webhook-secret", flagGitHubWebhookSecret, "GitHub webhook secret")
-	flag.Int64Var(&flagGroupcache, "groupcache", flagGroupcache, "Groupcache")
-	flag.StringVar(&flagGroupcacheSelf, "groupcache-self", flagGroupcacheSelf, "Groupcache self")
-	flag.StringVar(&flagGroupcachePeers, "groupcache-peers", flagGroupcachePeers, "Groupcache peers")
-	flag.Int64Var(&flagCacheMemory, "cache-memory", flagCacheMemory, "Cache memory")
+	flag.Int64Var(&flagCache, "cache", flagCache, "Cache")
 	flag.Parse()
 }
 
-func initLog() {
-	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
-	log.SetOutput(os.Stdout)
-	log.Println("Start")
-}
-
-func logEnv() {
-	log.Printf("Go version: %s", runtime.Version())
-	log.Printf("Go max procs: %d", runtime.GOMAXPROCS(0))
-}
-
 func startHTTPServer() {
-	log.Printf("Start HTTP server on %s", flagHTTPAddr)
 	err := http.ListenAndServe(flagHTTPAddr, newHTTPHandler())
 	if err != nil {
-		log.Panic(err)
+		panic(err)
 	}
 }
 
@@ -95,44 +63,7 @@ func newHTTPHandler() http.Handler {
 	if h := newGitHubWebhookHTTPHandler(); h != nil {
 		mux.Handle("/github_webhook", h)
 	}
-	h := newLoggerHTTPHandler(mux)
-	return h
-}
-
-func newLoggerHTTPHandler(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		lrw := &logResponseWriter{ResponseWriter: rw}
-		start := time.Now()
-		h.ServeHTTP(lrw, req)
-		log.Printf(
-			"HTTP %s %s (%s %s) => %d %d %s",
-			req.Method, req.URL,
-			req.RemoteAddr, req.UserAgent(),
-			lrw.Code, lrw.Size, time.Since(start),
-		)
-	})
-}
-
-type logResponseWriter struct {
-	http.ResponseWriter
-	Code int
-	Size int
-}
-
-func (rw *logResponseWriter) WriteHeader(code int) {
-	rw.ResponseWriter.WriteHeader(code)
-	rw.Code = code
-}
-
-func (rw *logResponseWriter) Write(b []byte) (int, error) {
-	if rw.Code == 0 {
-		rw.WriteHeader(http.StatusOK)
-	}
-	size, err := rw.ResponseWriter.Write(b)
-	if err == nil {
-		rw.Size += size
-	}
-	return size, err
+	return mux
 }
 
 func newGitHubWebhookHTTPHandler() http.Handler {
@@ -142,18 +73,11 @@ func newGitHubWebhookHTTPHandler() http.Handler {
 	return &githubhook.Handler{
 		Secret: flagGitHubWebhookSecret,
 		Delivery: func(event string, deliveryID string, payload interface{}) {
-			log.Printf("Received GitHub webhook: %s", event)
 			if event == "push" {
-				delay := 5 * time.Second
-				log.Printf("Killing process in %s", delay)
-				time.AfterFunc(delay, func() {
-					log.Println("Killing process now")
+				time.AfterFunc(5*time.Second, func() {
 					os.Exit(0)
 				})
 			}
-		},
-		Error: func(err error, req *http.Request) {
-			log.Printf("GitHub webhook error: %s", err)
 		},
 	}
 }
@@ -172,9 +96,6 @@ func newImageHTTPHandler() http.Handler {
 		}),
 		Server:   newServer(),
 		ETagFunc: imageserver_http.NewParamsHashETagFunc(sha256.New),
-		ErrorFunc: func(err error, req *http.Request) {
-			log.Printf("Internal error: %s", err)
-		},
 	}
 	handler = &imageserver_http.ExpiresHandler{
 		Handler: handler,
@@ -190,7 +111,6 @@ func newServer() imageserver.Server {
 	srv := imageserver_testdata.Server
 	srv = newServerImage(srv)
 	srv = newServerLimit(srv)
-	srv = newServerGroupcache(srv)
 	srv = newServerCacheMemory(srv)
 	return srv
 }
@@ -240,102 +160,13 @@ func newServerLimit(srv imageserver.Server) imageserver.Server {
 	return imageserver.NewLimitServer(srv, runtime.GOMAXPROCS(0)*2)
 }
 
-func newServerGroupcache(srv imageserver.Server) imageserver.Server {
-	if flagGroupcache <= 0 {
-		return srv
-	}
-	return imageserver_cache_groupcache.NewServer(
-		srv,
-		imageserver_cache.NewParamsHashKeyGenerator(sha256.New),
-		groupcacheName,
-		flagGroupcache,
-	)
-}
-
 func newServerCacheMemory(srv imageserver.Server) imageserver.Server {
-	if flagCacheMemory <= 0 {
+	if flagCache <= 0 {
 		return srv
 	}
 	return &imageserver_cache.Server{
 		Server:       srv,
-		Cache:        imageserver_cache_memory.New(flagCacheMemory),
+		Cache:        imageserver_cache_memory.New(flagCache),
 		KeyGenerator: imageserver_cache.NewParamsHashKeyGenerator(sha256.New),
-	}
-}
-
-func startGroupcacheHTTPServer() {
-	if flagGroupcacheSelf == "" {
-		return
-	}
-	go func() {
-		log.Printf("Start groupcache HTTP server on %s", flagGroupcacheSelf)
-		err := http.ListenAndServe(flagGroupcacheSelf, newGroupcacheHTTPHandler())
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
-}
-
-func newGroupcacheHTTPHandler() http.Handler {
-	mux := http.NewServeMux()
-	mux.Handle("/", newGroupcacheHTTPPool())
-	mux.HandleFunc("/stats", groupcacheStatsHTTPHandler)
-	mux.Handle("/favicon.ico", http.NotFoundHandler())
-	return mux
-}
-
-func newGroupcacheHTTPPool() *groupcache.HTTPPool {
-	self := (&url.URL{Scheme: "http", Host: flagGroupcacheSelf}).String()
-	var peers []string
-	peers = append(peers, self)
-	for _, p := range strings.Split(flagGroupcachePeers, ",") {
-		if p == "" {
-			continue
-		}
-		peer := (&url.URL{Scheme: "http", Host: p}).String()
-		peers = append(peers, peer)
-	}
-	opts := &groupcache.HTTPPoolOptions{BasePath: "/"}
-	pool := groupcache.NewHTTPPoolOpts(self, opts)
-	pool.Context = imageserver_cache_groupcache.HTTPPoolContext
-	pool.Transport = imageserver_cache_groupcache.NewHTTPPoolTransport(nil)
-	pool.Set(peers...)
-	return pool
-}
-
-func groupcacheStatsHTTPHandler(w http.ResponseWriter, req *http.Request) {
-	gp := groupcache.GetGroup(groupcacheName)
-	if gp == nil {
-		http.Error(w, fmt.Sprintf("group %s not found", groupcacheName), http.StatusServiceUnavailable)
-		return
-	}
-	type cachesStats struct {
-		Main groupcache.CacheStats
-		Hot  groupcache.CacheStats
-	}
-	type stats struct {
-		Group  groupcache.Stats
-		Caches cachesStats
-	}
-	data, err := json.MarshalIndent(
-		stats{
-			Group: gp.Stats,
-			Caches: cachesStats{
-				Main: gp.CacheStats(groupcache.MainCache),
-				Hot:  gp.CacheStats(groupcache.HotCache),
-			},
-		},
-		"",
-		"	",
-	)
-	if err != nil {
-		log.Println(err)
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-	_, err = w.Write(data)
-	if err != nil {
-		log.Println(err)
-		return
 	}
 }
---
+++ examples/groupcache/groupcache.go
@@ -0,0 +1,160 @@
+// Package advanced provides a groupcache example.
+//
+// Run:
+//  go run groupcache.go -http=:8081 -groupcache-peers=:8082,:8083
+//  go run groupcache.go -http=:8082 -groupcache-peers=:8081,:8083
+//  go run groupcache.go -http=:8083 -groupcache-peers=:8081,:8082
+//
+// Open http://localhost:8081/medium.jpg?width=100
+//
+// Stats are available on http://localhost:8081/stats
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/disintegration/gift"
+	"github.com/golang/groupcache"
+	"github.com/pierrre/imageserver"
+	imageserver_cache "github.com/pierrre/imageserver/cache"
+	imageserver_cache_groupcache "github.com/pierrre/imageserver/cache/groupcache"
+	imageserver_http "github.com/pierrre/imageserver/http"
+	imageserver_http_gift "github.com/pierrre/imageserver/http/gift"
+	imageserver_http_image "github.com/pierrre/imageserver/http/image"
+	imageserver_image "github.com/pierrre/imageserver/image"
+	_ "github.com/pierrre/imageserver/image/gif"
+	imageserver_image_gift "github.com/pierrre/imageserver/image/gift"
+	_ "github.com/pierrre/imageserver/image/jpeg"
+	_ "github.com/pierrre/imageserver/image/png"
+	imageserver_testdata "github.com/pierrre/imageserver/testdata"
+)
+
+const (
+	groupcacheName = "imageserver"
+)
+
+var (
+	flagHTTPAddr        = ":8080"
+	flagGroupcache      = int64(128 * (1 << 20))
+	flagGroupcachePeers string
+)
+
+func main() {
+	parseFlags()
+	startHTTPServer()
+}
+
+func parseFlags() {
+	flag.StringVar(&flagHTTPAddr, "http", flagHTTPAddr, "HTTP addr")
+	flag.Int64Var(&flagGroupcache, "groupcache", flagGroupcache, "Groupcache")
+	flag.StringVar(&flagGroupcachePeers, "groupcache-peers", flagGroupcachePeers, "Groupcache peers")
+	flag.Parse()
+}
+
+func startHTTPServer() {
+	http.Handle("/", http.StripPrefix("/", newImageHTTPHandler()))
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	initGroupcacheHTTPPool() // it automatically registers itself to "/_groupcache"
+	http.HandleFunc("/stats", groupcacheStatsHTTPHandler)
+	err := http.ListenAndServe(flagHTTPAddr, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func newImageHTTPHandler() http.Handler {
+	return &imageserver_http.Handler{
+		Parser: imageserver_http.ListParser([]imageserver_http.Parser{
+			&imageserver_http.SourcePathParser{},
+			&imageserver_http_gift.ResizeParser{},
+			&imageserver_http_image.FormatParser{},
+			&imageserver_http_image.QualityParser{},
+		}),
+		Server: newServer(),
+	}
+}
+
+func newServer() imageserver.Server {
+	srv := imageserver_testdata.Server
+	srv = newServerImage(srv)
+	srv = newServerGroupcache(srv)
+	return srv
+}
+
+func newServerImage(srv imageserver.Server) imageserver.Server {
+	return &imageserver.HandlerServer{
+		Server: srv,
+		Handler: &imageserver_image.Handler{
+			Processor: &imageserver_image_gift.ResizeProcessor{
+				DefaultResampling: gift.LanczosResampling,
+			},
+		},
+	}
+}
+
+func newServerGroupcache(srv imageserver.Server) imageserver.Server {
+	if flagGroupcache <= 0 {
+		return srv
+	}
+	return imageserver_cache_groupcache.NewServer(
+		srv,
+		imageserver_cache.NewParamsHashKeyGenerator(sha256.New),
+		groupcacheName,
+		flagGroupcache,
+	)
+}
+
+func initGroupcacheHTTPPool() {
+	self := (&url.URL{Scheme: "http", Host: flagHTTPAddr}).String()
+	var peers []string
+	peers = append(peers, self)
+	for _, p := range strings.Split(flagGroupcachePeers, ",") {
+		if p == "" {
+			continue
+		}
+		peer := (&url.URL{Scheme: "http", Host: p}).String()
+		peers = append(peers, peer)
+	}
+	pool := groupcache.NewHTTPPool(self)
+	pool.Context = imageserver_cache_groupcache.HTTPPoolContext
+	pool.Transport = imageserver_cache_groupcache.NewHTTPPoolTransport(nil)
+	pool.Set(peers...)
+}
+
+func groupcacheStatsHTTPHandler(w http.ResponseWriter, req *http.Request) {
+	gp := groupcache.GetGroup(groupcacheName)
+	if gp == nil {
+		http.Error(w, fmt.Sprintf("group %s not found", groupcacheName), http.StatusServiceUnavailable)
+		return
+	}
+	type cachesStats struct {
+		Main groupcache.CacheStats
+		Hot  groupcache.CacheStats
+	}
+	type stats struct {
+		Group  groupcache.Stats
+		Caches cachesStats
+	}
+	data, err := json.MarshalIndent(
+		stats{
+			Group: gp.Stats,
+			Caches: cachesStats{
+				Main: gp.CacheStats(groupcache.MainCache),
+				Hot:  gp.CacheStats(groupcache.HotCache),
+			},
+		},
+		"",
+		"	",
+	)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	w.Write(data)
+}
