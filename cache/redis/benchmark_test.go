20474c7fcccb38fd9b35b27d1eaeb10e1b34c1c4
Pierre Durand <pierredurand@gmail.com>
2016-05-19 14:55:38 +0200

    cache/redis: fix benchmark parallelism

--- cache/redis/benchmark_test.go
+++ cache/redis/benchmark_test.go
@@ -67,5 +67,5 @@ func benchmarkGetParallelism(b *testing.B, parallelism int) {
 func benchmarkGet(b *testing.B, image *imageserver.Image, parallelism int) {
 	cache := newTestCache(b)
 	defer cache.Pool.Close()
-	cachetest.BenchmarkGet(b, cache, 16, image)
+	cachetest.BenchmarkGet(b, cache, parallelism, image)
 }
