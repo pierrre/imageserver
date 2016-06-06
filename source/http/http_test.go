4f73abb707eca6fe2cc6be6bb819dff93ade0187
Pierre Durand <pierredurand@gmail.com>
2016-06-06 17:11:30 +0200

    source/http: refactor tests: use test cases instead of functions

--- source/http/http_test.go
+++ source/http/http_test.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -14,78 +15,71 @@ import (
 var _ imageserver.Server = &Server{}
 
 func TestGet(t *testing.T) {
-	httpSrv := createTestHTTPServer(t)
+	srv := &Server{}
+	httpSrv := createTestHTTPServer()
 	defer httpSrv.Close()
-	params := imageserver.Params{
-		imageserver_source.Param: createTestSource(httpSrv, testdata.MediumFileName),
-	}
-	srv := &Server{}
-	im, err := srv.Get(params)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if im == nil {
-		t.Fatal("no image")
-	}
-	if im.Format != testdata.Medium.Format {
-		t.Fatalf("unexpected image format: got \"%s\", want \"%s\"", im.Format, testdata.Medium.Format)
-	}
-	if len(im.Data) != len(testdata.Medium.Data) {
-		t.Fatalf("unexpected image data length: got %d, want %d", len(im.Data), len(testdata.Medium.Data))
-	}
-}
-
-func TestGetErrorNoSource(t *testing.T) {
-	httpSrv := createTestHTTPServer(t)
-	defer httpSrv.Close()
-	params := imageserver.Params{}
-	srv := &Server{}
-	_, err := srv.Get(params)
-	if err == nil {
-		t.Fatal("no error")
-	}
-	if _, ok := err.(*imageserver.ParamError); !ok {
-		t.Fatalf("unexpected error type: %T", err)
-	}
-}
-
-func TestGetErrorNewRequest(t *testing.T) {
-	params := imageserver.Params{imageserver_source.Param: "%"}
-	srv := &Server{}
-	_, err := srv.Get(params)
-	if err == nil {
-		t.Fatal("no error")
-	}
-	if _, ok := err.(*imageserver.ParamError); !ok {
-		t.Fatalf("unexpected error type: %T", err)
-	}
-}
-
-func TestGetErrorDoRequest(t *testing.T) {
-	params := imageserver.Params{imageserver_source.Param: "http://localhost:123456"}
-	srv := &Server{}
-	_, err := srv.Get(params)
-	if err == nil {
-		t.Fatal("no error")
-	}
-	if _, ok := err.(*imageserver.ParamError); !ok {
-		t.Fatalf("unexpected error type: %T", err)
-	}
-}
-
-func TestGetErrorNotFound(t *testing.T) {
-	httpSrv := createTestHTTPServer(t)
-	defer httpSrv.Close()
-	source := createTestSource(httpSrv, testdata.MediumFileName)
-	source += "foobar"
-	params := imageserver.Params{imageserver_source.Param: source}
-	srv := &Server{}
-	_, err := srv.Get(params)
-	if err == nil {
-		t.Fatal("no error")
-	}
-	if _, ok := err.(*imageserver.ParamError); !ok {
-		t.Fatalf("unexpected error type: %T", err)
+	for _, tc := range []struct {
+		name               string
+		params             imageserver.Params
+		expectedParamError string
+		expectedImage      *imageserver.Image
+	}{
+		{
+			name: "Normal",
+			params: imageserver.Params{
+				imageserver_source.Param: createTestSource(httpSrv, testdata.MediumFileName),
+			},
+			expectedImage: testdata.Medium,
+		},
+		{
+			name:               "NoSource",
+			params:             imageserver.Params{},
+			expectedParamError: imageserver_source.Param,
+		},
+		{
+			name: "InvalidURL",
+			params: imageserver.Params{
+				imageserver_source.Param: "%",
+			},
+			expectedParamError: imageserver_source.Param,
+		},
+		{
+			name: "UnreachableURL",
+			params: imageserver.Params{
+				imageserver_source.Param: "http://localhost:123456",
+			},
+			expectedParamError: imageserver_source.Param,
+		},
+		{
+			name: "NotFound",
+			params: imageserver.Params{
+				imageserver_source.Param: createTestSource(httpSrv, testdata.MediumFileName) + "foobar",
+			},
+			expectedParamError: imageserver_source.Param,
+		},
+	} {
+		func() {
+			t.Logf("test: %s", tc.name)
+			im, err := srv.Get(tc.params)
+			if err != nil {
+				if err, ok := err.(*imageserver.ParamError); ok && err.Param == tc.expectedParamError {
+					return
+				}
+				t.Fatal(err)
+			}
+			if tc.expectedParamError != "" {
+				t.Fatal("no error")
+			}
+			if im == nil {
+				t.Fatal("no image")
+			}
+			if im.Format != tc.expectedImage.Format {
+				t.Fatalf("unexpected image format: got \"%s\", want \"%s\"", im.Format, tc.expectedImage.Format)
+			}
+			if !bytes.Equal(im.Data, tc.expectedImage.Data) {
+				t.Fatal("data not equal")
+			}
+		}()
 	}
 }
 
@@ -113,7 +107,7 @@ func TestParseResponseErrorData(t *testing.T) {
 	}
 }
 
-func createTestHTTPServer(t *testing.T) *httptest.Server {
+func createTestHTTPServer() *httptest.Server {
 	return httptest.NewServer(http.FileServer(http.Dir(testdata.Dir)))
 }
 
